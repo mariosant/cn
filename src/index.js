13a7d0c5201451812d55153a2e3acf11d785c403
Marios Antonoudiou <mariosant@sent.com>
2019-12-17 10:30:22 +0200

    Fix linting

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const handleTuple = ([conditional, yeap, nope]) => (conditional ? yeap : nope);
 const handleString = c => c;
 
 const reducer = (acc, item) =>
-  [acc, Array.isArray(item) ? handleTuple(item) : handleString(item)]
-    .join(" ")
-    .trim();
+	[acc, Array.isArray(item) ? handleTuple(item) : handleString(item)]
+		.join(' ')
+		.trim();
 
 const handler = (...cases) => cases.reduce(reducer, undefined);
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,22 @@
-import className from "./index";
+import className from './index';
 
-test("outputs single class", () => {
-  expect(className("test")).toEqual("test");
+test('outputs single class', () => {
+	expect(className('test')).toEqual('test');
 });
 
-test("outputs multiple classes", () => {
-  expect(className("test", "work")).toEqual("test work");
+test('outputs multiple classes', () => {
+	expect(className('test', 'work')).toEqual('test work');
 });
 
-test("works with conditions", () => {
-  expect(className([true, "works", "does not work"])).toEqual("works");
-  expect(className([false, "works", "does not work"])).toEqual("does not work");
+test('works with conditions', () => {
+	expect(className([true, 'works', 'does not work'])).toEqual('works');
+	expect(className([false, 'works', 'does not work'])).toEqual(
+		'does not work'
+	);
 });
 
-test("a mixed example", () => {
-  expect(className("a", "b", [true, "works", "does not work"])).toEqual(
-    "a b works"
-  );
+test('a mixed example', () => {
+	expect(className('a', 'b', [true, 'works', 'does not work'])).toEqual(
+		'a b works'
+	);
 });
